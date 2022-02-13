02349129041604b6a637d7c25b9f851570d39112
Rajat Sharma <lunasunkaiser@gmail.com>
2022-02-13 18:06:35 +0530

    Allow install multiple scripts

--- app/Kelp/Get.hs
+++ app/Kelp/Get.hs
@@ -3,12 +3,15 @@
 module Kelp.Get where
 
 import Data.String (IsString (fromString))
+import Data.UUID.V4 (nextRandom)
+import Soothsayer ((***))
 import System.Directory (getHomeDirectory)
-import System.Process (createProcess, proc)
+import System.Process (callProcess)
 
 run :: String -> IO ()
 run name = do
   home <- getHomeDirectory
-  let process = proc "curl" [name, "-o", home ++ "/.kelp/private.fish"]
-  _ <- createProcess process
-  pure ()
+  uuid <- nextRandom
+  let destination = "{0}/.kelp/{1}" *** [home, show uuid]
+  callProcess "mkdir" ["-p", destination]
+  callProcess "curl" [name, "-o", "{0}/private.fish" *** [destination]]
--- app/Kelp/Init.hs
+++ app/Kelp/Init.hs
@@ -4,14 +4,13 @@ module Kelp.Init where
 
 import Data.String (IsString (fromString))
 import System.Directory (getHomeDirectory)
-import System.Process (createProcess, proc)
+import System.Process (callProcess)
 
 run :: IO ()
 run = do
   home <- getHomeDirectory
-  let process = proc "mkdir" ["-p", home ++ "/.kelp"]
-  _ <- createProcess process
+  callProcess "mkdir" ["-p", home ++ "/.kelp"]
   let shellScript =
         unlines
-          ["for file in ~/.kelp/*.fish", "\t[ -r \"$file\" ] && [ -f \"$file\" ] && source \"$file\";", "end"]
+          ["for file in ~/.kelp/**/*.fish", "\t[ -r \"$file\" ] && [ -f \"$file\" ] && source \"$file\";", "end"]
   putStr shellScript
